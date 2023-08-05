8545b2f530077ac3f8268425c84b348c7b6da65a
karoui souha <karoui.souha18@gmail.com>
2023-08-05 08:24:11 +0200

    Zookeeper

---
+++ OOP/OOP_Advanced/core/ZooKeper/TestGorilla.java
@@ -0,0 +1,16 @@
+package com.souha.ZooKeper;
+
+public class TestGorilla {
+
+	public static void main(String[] args) {
+		Gorilla gorilla =new Gorilla();
+        gorilla.throw_thing(); 
+        gorilla.throw_thing(); 
+        gorilla.throw_thing(); 
+        gorilla.eatBananas();
+        gorilla.eatBananas();
+        gorilla.climb();
+        gorilla.displayEnergy();
+	}
+
+}
